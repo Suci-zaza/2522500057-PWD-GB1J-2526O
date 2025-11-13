b28869e56d6783de97aae463d7a843ed600f93c3
unknown <2511500081@mahasiswa.atmaluhur.ac.id>
2025-11-13 18:11:18 +0700

    UBAH PROSES.PHP

--- pertemuan-08/bahan UTS PWD/proses.php
+++ pertemuan-08/bahan UTS PWD/proses.php
@@ -1,5 +1,28 @@
 <?php
 session_start();
+$sesNIM = $_POST["txtNIM"];
+$sesNamalengkap = $_POST["txtNamalengkap"];
+$sesTempatlahir = $_POST["txtTempatlahir"];
+$sesTanggallahir = $_POST["txtTanggallahir"];
+$sesHobi = $_POST["txtHobi"];
+$sesPasangan = $_POST["txtPasangan"];
+$sesPekerjaan= $_POST["txtPekerjaan"];
+$sesNamakakak= $_POST["txtNamakakak"];
+$sesNamaadik = $_POST["txtNamaadik"];
+$_SESSION["sesNIM"] = $sesNIM;
+$_SESSION["sesNamalengkap"] = $sesNamalengkap;
+$_SESSION["sesTempatlahir "] = $sesTempatlahir ;
+$_SESSION["sesTanggallahir"] = $sesTanggallahir;
+$_SESSION["sesHobi"] = $sesHobi;
+$_SESSION["sesPasangan"] = $sesPasangan;
+$_SESSION["sesNamakakak"] = $sesNamakakak;
+$_SESSION["sesNamaadik"] = $sesNamaadik;
+$_SESSION["sesPekerjaan"] = $sesPekerjaan;
+header("location: index.php");
+?>
+
+<?php
+session_start();
 $sesnama = $_POST["txtNama"];
 $sesemail = $_POST["txtEmail"];
 $sespesan = $_POST["txtPesan"];
